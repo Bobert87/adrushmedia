ec053ea98e36e672d2b20cac30ed7ae4b808c409
Boris Ruiz <boris.rp@gmail.com>
2024-05-29 21:51:56 -0600

    Geo targeting now picks ads, need to determine how to sort them per schedule

--- api/src/controllers/core/schedule.js
+++ api/src/controllers/core/schedule.js
@@ -3,6 +3,7 @@ const campaignModel = require('../../models/demand/campaign');
 const deviceModel = require('../../models/supply/device');
 const polygonModel = require('../../models/core/polygon');
 const zoneModel = require('../../models/core/zone');
+const adModel = require('../../models/demand/ad');
 const h3 = require('h3-js');
 
 class Schedule {
@@ -12,6 +13,7 @@ class Schedule {
         this.deviceModel = new deviceModel();
         this.polygonModel = new polygonModel();
         this.zoneModel = new zoneModel();
+        this.adsModel = new adModel();
     }
 
     async getDevice(req) {
@@ -32,16 +34,10 @@ class Schedule {
         return device;
     }
 
-    buildZones(campaigns) {
-        const zones = []
-        campaigns.forEach(campaign => {
-            campaign.filters.forEach(filter => {
-                if (filter.type === 'GEO') {
-                    zones.push({ campaignId: campaign.id, zoneId: filter.value, operation: filter.operation })
-                }
-            });
-        });
-        return zones;
+    campaignsWithGeoFilter(campaigns) {
+        return campaigns.filter(campaign => campaign.filters.some(filter => {            
+            return filter.type === 'GEO' && filter.operation === 'IN'
+        }))
     }
 
     filterZones(zones, device) {
@@ -75,6 +71,20 @@ class Schedule {
         return formatedZones;
     }
 
+    getCampaignZoneDictionary(campaigns) {
+        const zones = [];
+        campaigns.forEach(campaign => {
+            campaign.filters.forEach(filter => {                    
+                if (filter.type === 'GEO' && filter.operation === 'IN') {                        
+                    zones.push({campaign, zoneId:parseInt(filter.value)});
+                }
+            });
+            
+        });
+        return zones;
+
+    }
+
     async needsNewSchedule(device){
         //discard creating  new schedule if the last was served less than x time ago.
         const currentTime = new Date();
@@ -84,6 +94,46 @@ class Schedule {
             return false;
         }
         return true;
+    }
+
+    floatArrayToLatLngArray(floatArray){
+        const latLngArray = [];
+        for (let i = 0; i < floatArray.length; i = i + 2) {
+            const element = [floatArray[i + 1], floatArray[i]];
+            latLngArray.push(element);
+        }
+        return latLngArray;
+    }
+
+    // This should be introduce with cache as it is a heavy operation
+    // that will rarely change its values over time.
+    async generateH3HexesByZone(zoneIds){
+        const allZones = await this.zoneModel.getByIds(zoneIds, 'areas');
+        const h3HexByZone = {};
+        allZones.forEach(zone => {            
+            zone.areas.forEach(area => {
+                const latLngArray = this.floatArrayToLatLngArray(area.polygon.coordinates);
+                const h3Hexes = h3.polygonToCells(latLngArray, 8);
+                h3HexByZone[zone.id] = h3HexByZone[zone.id] || [];
+                h3HexByZone[zone.id].push(h3Hexes);
+            });
+        });
+        return h3HexByZone;
+    }
+
+    async deviceInZone(device, campaignZoneDictionary){
+        const campaigns = {};
+        const h3Areas = await this.generateH3HexesByZone(campaignZoneDictionary.map(campaignZone => campaignZone.zoneId));
+        const h3Device = h3.latLngToCell(device.location.lat, device.location.lng, 8);
+        for (const zoneId in h3Areas) {
+            if (h3Areas[zoneId].some(h3Hex => h3Hex.indexOf(h3Device) > -1))
+                campaignZoneDictionary.forEach(campaignZone => {
+                    if (campaignZone.zoneId === parseInt(zoneId))
+                        if (!campaigns[campaignZone.campaign.id])
+                            campaigns[campaignZone.campaign.id] = campaignZone.campaign;
+                })
+        }
+        return campaigns;
     }
 
     async create(req, res) {
@@ -98,22 +148,42 @@ class Schedule {
                 res.status(400).json({ error: "Device is already scheduled" });
 
             // Will only work with active campaings.
-            const activeCampaigns = await this.campaignModel.getByStatus('ACTIVE', 'filters');
+            const activeCampaigns = await this.campaignModel.getByStatus('ACTIVE', 'filters,ads');
 
-            // list all zones that appear on campaigns as filters
-            const zonesOnCampaignFilters = this.buildZones(activeCampaigns);
+            // This constructs a dictionary with the campaign and the zone that has a geo filter
+            const campaignZoneDictionary = this.getCampaignZoneDictionary(activeCampaigns)            
 
-            // a function from the model that gets all zones by id in single query.
-            const allZones = await this.zoneModel.getByIds(zonesOnCampaignFilters.map(g => parseInt(g.zoneId)), 'areas');
+            const geoFilteredCampaigns = await this.deviceInZone(device,campaignZoneDictionary);
+            
+            const ads = [];
+            for (const campaignId in geoFilteredCampaigns) {
+                const campaign = geoFilteredCampaigns[campaignId];
+                campaign.ads.forEach(ad => {
+                    if (ad.status !== 'ACTIVE')
+                        return;                    
+                    const maxBid = campaign.maxBid;
+                    const newAd = {...ad, revenue: maxBid * ad.duration/60};
+                    if (newAd.targetDeviceType === device.type)
+                        ads.push(newAd);
+                });
+            }            
 
-            // same zones as below polygons are formated in [[x,y],[x,y]] format
-            const formatedZones = this.formatAreas(allZones);
+            ads.sort((a, b) => b.revenue - a.revenue);
 
-            // filter only the zones that are IN the device location
-            const zones = this.filterZones(formatedZones, device);
+        
 
-            if (zones.length > 0)
-                res.json({ message: 'Campaigns were found' })
+            // activeCampaigns.filter(campaign => {
+            //     filters.forEach(zone => {
+            //         if (campaign.id === zone.campaignId) {
+            //             // create a schedule for the device
+            //             this.scheduleModel.create(device.id, campaign.id, zone.id);
+            //         }
+            //     });
+            
+            // })
+
+            if (ads)
+                res.json({ ads})
 
         } catch (error) {
             res.status(500).json({ error: error.message });
